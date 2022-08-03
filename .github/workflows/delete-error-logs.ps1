bd9591d6d3cf88bea92a4df85b4b61ff1b79ff79
degnoskeVA <103440839+degnoskeVA@users.noreply.github.com>
2022-08-03 11:55:55 -0500

    Update delete-error-logs.ps1

--- .github/workflows/delete-error-logs.ps1
+++ .github/workflows/delete-error-logs.ps1
@@ -1,5 +1,5 @@
 param($username)
 Write-Host "Deleting Error Logs" 
-sfdx config:set maxQueryLimit=200000
+sfdx config:set maxQueryLimit=200000 -g
 sfdx force:data:soql:query -q "SELECT Id FROM ACMN_Error_Log__c" --resultformat csv -u $username  | Out-File -encoding oem out.csv 
 sfdx force:data:bulk:delete -s ACMN_Error_Log__c -f out.csv -u  $username 
